abfdad3ac0378de46e4555b967c0e830009efa4f
qhanfckdrh <suribin.kim@gmail.com>
2021-10-13 22:21:35 +0900

    [bugfix] check the the length of car name
    - car name should be equal or less than length 5

--- src/main/java/racinggame/model/Car.java
+++ src/main/java/racinggame/model/Car.java
@@ -1,11 +1,20 @@
 package racinggame.model;
 
+import racinggame.view.Message;
+
 public class Car {
     private String name;
     private int position;
 
     public void setName(String name){
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name){
+        if (name.length() > 5){
+            Message.printMessage(Message.ERROR + "자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 
     public void setPosition(int value){
